f690b595c977a6a0aa87188324bd766c98d4be62
Nando Vieira <fnando.vieira@gmail.com>
2015-10-21 21:11:15 -0200

    Add whitelist support.

--- inject.js
+++ inject.js
@@ -1,7 +1,18 @@
 (function(){
+  "use strict";
+
   var each = Array.prototype.forEach;
+  var whitelist = [];
+  var tid;
 
   function cleanup() {
+    var hostname = window.top.location.hostname;
+    var allowed = whitelist.indexOf(hostname) !== -1;
+
+    if (allowed) {
+      return;
+    }
+
     var links = document.querySelectorAll("a[target]");
     each.call(links, function(link) {
       if (link.target === "_blank" || link.target == "_new") {
@@ -10,6 +21,12 @@
     });
   }
 
-  cleanup();
-  setInterval(cleanup, 5000);
+  safari.self.addEventListener("message", function(event) {
+    whitelist = event.message;
+    cleanup();
+    clearInterval(tid);
+    tid = setInterval(cleanup, 5000);
+  }, false);
+
+  safari.self.tab.dispatchMessage("whitelist");
 })();
